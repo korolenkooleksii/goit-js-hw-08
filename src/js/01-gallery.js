a459d3c3773529dd81c6e8bf66ed7aa1a4ea5930
Oleksii Korolenko <as_vostok@i.ua>
2023-01-23 22:04:32 +0200

    add task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,34 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+// console.log(galleryItems);
 
-console.log(galleryItems);
-console.log(galleryItems);
+const galleryList = document.querySelector('.gallery');
+const galleryMarkup = createGallaryItemsMarkup(galleryItems);
+
+galleryList.insertAdjacentHTML('beforeend', galleryMarkup);
+
+galleryList.addEventListener('click', onBlockStandartAction);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  scrollZoom: false,
+});
+
+function createGallaryItemsMarkup(items) {
+  return items
+    .map(({ description, preview, original }) => {
+      return `<a class="gallery__item" href="${original}">
+          <img
+            class="gallery__image" src="${preview}" alt="${description}" />
+        </a>`;
+    })
+    .join('');
+}
+
+function onBlockStandartAction(e) {
+  e.preventDefault();
+}
