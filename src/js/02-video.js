8212262af204b3853b5f5755da7aada6d7e05c24
Oleksii Korolenko <as_vostok@i.ua>
2023-01-25 17:19:41 +0200

    add task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,26 @@
+import Player from '@vimeo/player';
+// const Player = require('@vimeo/player');
+const throttle = require('lodash.throttle');
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+console.log(localStorage);
+
+const onPlay = function ({ seconds }) {
+  try {
+    const currentTime = seconds;
+    localStorage.setItem('videoplayer-current-time', currentTime);
+  } catch (error) {
+    console.error('Set state error: ', error.message);
+  }
+};
+
+const throttlePlay = throttle(onPlay, 1000);
+
+player.on('timeupdate', throttlePlay);
+
+
+// player.getVideoTitle().then(function (title) {
+//   console.log('title:', title);
+// });
